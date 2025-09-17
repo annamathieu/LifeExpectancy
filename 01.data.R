8b7a60439edaa673cd6fd5d0375665c321afcd75
Anna Mathieu <annamathieupro@gmail.com>
2025-09-17 15:40:19 +0200

    data import

--- 01.data.R
+++ 01.data.R
@@ -1,2 +1,11 @@
 
 # Importation des données 
+library(dplyr)
+library(tidyverse)
+
+life_expectancy <- read.csv("life_expectancy.csv", sep=",", dec=".")
+
+str(life_expectancy)
+
+summary(life_expectancy)
+
