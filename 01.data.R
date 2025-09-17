929166aa9eab336158db6e571b31343f24c020ec
dtraore40 <traoreallwf@gmail.com>
2025-09-17 16:01:52 +0200

    commit 2

--- 01.data.R
+++ 01.data.R
@@ -1,5 +1,21 @@
 
 # Importation des données 
 
+library(dplyr)
+library(tidyverse)
 
-# test 2
+life_expectancy <- read.csv("life_expectancy.csv", sep=",", dec=".")
+
+str(life_expectancy)
+
+summary(life_expectancy)
+
+# Vérification du type des variables 
+life_expectancy$Country <- as.factor(life_expectancy$Country)
+
+life_expectancy %>% mutate(
+  "Pays" = "Country",
+  "Annees" = "Year",
+  "Status" = "Status"         
+  
+)
